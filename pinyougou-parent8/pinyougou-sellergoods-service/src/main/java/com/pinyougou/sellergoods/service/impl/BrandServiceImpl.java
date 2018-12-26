8796e9f13d8dc3fa46e76255e732489bce8bf8d9
ffyehuan <121779696@qq.com>
2018-12-26 15:04:29 +0800

    pyg-----day01

---
+++ pinyougou-parent8/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,24 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @createTime: 2018-12-26 13:07
+ */
+
+@Service
+public class BrandServiceImpl implements BrandService {
+    @Autowired
+    private TbBrandMapper brandMapper;
+    @Override
+    public List<TbBrand> findAll() {
+        return brandMapper.selectByExample(null);
+    }
+}
